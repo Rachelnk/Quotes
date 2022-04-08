4a8ddb34f0efd250c4f10ca42a0845ef67b05155
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-08 16:45:22 +0300

    Add constructor function in quote class.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
+  id:number;
+  quote:string;
+  upvote:number;
+  downvote:number;
+  constructor (id:number, quote:string, upvote:number, downvote:number) {
+    this.id =id;
+    this.quote = quote;
+    this.upvote = upvote;
+    this.downvote = downvote;
+
+  }
 }
