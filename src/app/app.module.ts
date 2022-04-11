045ec3700b05ad852642073b3fd46d5f3c960150
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-11 12:58:44 +0300

    Add quote details component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesformComponent } from './quotesform/quotesform.component';
 import { HighlightDirective } from './highlight.directive';
+import { QuotedetailComponent } from './quotedetail/quotedetail.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HighlightDirective } from './highlight.directive';
     AppComponent,
     QuotesComponent,
     QuotesformComponent,
-    HighlightDirective
+    HighlightDirective,
+    QuotedetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
- 
+  
+  showDescription: boolean;
   constructor (public id:number, public quote:string, public quoteBy:string, public quoteContributor:string, public upvote:number, public downvote:number) {
     this.id =id;
     this.quote = quote;
@@ -7,6 +8,7 @@ export class Quote {
     this.quoteContributor = quoteContributor;
     this.upvote = upvote;
     this.downvote = downvote;
+    this.showDescription = false;
 
   }
 }
