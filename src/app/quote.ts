befa195d16080eea044945d91f04b547ee3b24a1
Rachel Kiarie <rachelnnk94@gmail.com>
2022-04-11 12:50:07 +0300

    Add quote contributor name.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,10 @@
 export class Quote {
  
-  constructor (public id:number, public quote:string, public quoteBy:string, public upvote:number, public downvote:number) {
+  constructor (public id:number, public quote:string, public quoteBy:string, public quoteContributor:string, public upvote:number, public downvote:number) {
     this.id =id;
     this.quote = quote;
     this.quoteBy = quoteBy;
+    this.quoteContributor = quoteContributor;
     this.upvote = upvote;
     this.downvote = downvote;
 
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,8 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   userquotes: Quote[]=[
-    new Quote (1, 'Float like a butterfly, sting like a bee.','Muhammad Ali',6, 2),
-    new Quote (2, 'When someone shows you who they are believe them the first time.','Maya Angelou',3,1),
-    new Quote (3, 'Lies run sprints, but the truth runs marathons.','Michael Jackson',10,3)
+    new Quote (1, 'Float like a butterfly, sting like a bee.','Muhammad Ali', "Rachel Kiarie",6, 2),
+    new Quote (3, 'Lies run sprints, but the truth runs marathons.','Michael Jackson',"Rachel Kiarie",10,3)
   ];
   addNewQuote(quotes:any){
     let quotelength = this.userquotes.length;
